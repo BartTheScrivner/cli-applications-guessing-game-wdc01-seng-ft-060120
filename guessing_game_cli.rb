fa701e7f30e9864f11f833e455ff7b08992c14e1
Dave Frame <dframe22@gmail.com>
2020-05-08 02:16:33 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,6 +8,6 @@ def run_guessing_game
   elsif guess == comp
     puts "You guessed the correct number!"
   else
-    puts "Sorry! The computer guessed #{comp}"
+    puts "Sorry! The computer guessed #{comp}."
   end
 end
